54486ed224ea4204028c607f5a558a9d5576b1c8
Nick Schuetz <githuber@thelinuxshack.com>
2017-11-01 14:15:31 -0500

    updated the gluster images to latest

--- ocp3.6/scripts/docker_pull.sh
+++ ocp3.6/scripts/docker_pull.sh
@@ -83,8 +83,8 @@ docker pull registry.access.redhat.com/rhscl/ruby-24-rhel7:2.4
 docker pull registry.access.redhat.com/rhscl/ror-42-rhel7:latest
 docker pull registry.access.redhat.com/rhscl/ror-42-rhel7:4.2
 
-docker pull registry.access.redhat.com/rhgs3/rhgs-server-rhel7:3.2.0-7
-docker pull registry.access.redhat.com/rhgs3/rhgs-volmanager-rhel7:3.2.0-11
+docker pull registry.access.redhat.com/rhgs3/rhgs-server-rhel7:latest
+docker pull registry.access.redhat.com/rhgs3/rhgs-volmanager-rhel7:latest
 
 docker pull docker.io/openshiftdemos/gogs:0.11.29
 docker pull docker.io/openshiftdemos/nexus:2.13.0-01
